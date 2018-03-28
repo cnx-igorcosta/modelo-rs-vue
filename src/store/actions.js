9e74b5e845fd1d98b57903db6f29456f4f7a4992
Igor Costa <igor.costa@wdev.int>
2018-03-28 12:50:00 -0300

    heroku deployment config

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,7 +6,7 @@ export const buscarModelos = ({commit}) => {
     // .then( response => response.resultados)
     // .then( resultados => commit(types.BUSCAR_MODELOS, resultados))
     // .catch( response => console.log(response))
-    commit(types.BUSCAR_MODELOS, [{_id:1, descricao: 'Criação de context-root', numero: 353275}])
+    commit(types.BUSCAR_MODELOS, getMockModelos())
 }
 
 export const detalharModelo = ({commit}, modeloRs) => {
@@ -31,4 +31,17 @@ export const limparModelo = ({commit}) => {
 
 export const novoModelo = ({commit}) => {
     commit(types.NOVO_MODELO)
+}
+
+function getMockModelos() {
+  return [
+    {_id:1, descricao: 'Criação de usuários na ferramenta BSRA', numero: 4651979},
+    {_id:2, descricao: 'Validação do BSRA da aplicação', numero: 4652693},
+    {_id:3, descricao: 'Inclusão de aplicação na plataforma IC', numero: 4667410},
+    {_id:4, descricao: 'Criação de Context Root da aplicação', numero: 4677590},
+    {_id:5, descricao: 'Grant em tabelas criadas', numero: 4698389},
+    {_id:5, descricao: 'Disponibilização de deploy automático no IC', numero: 4698389},
+    {_id:6, descricao: 'Criação de links na intranet', numero: 4705810},
+    {_id:6, descricao: 'Habilitar log de produção no IC', numero: 4590765},
+  ]
 }
